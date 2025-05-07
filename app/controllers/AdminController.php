b07f0e6d5ca9868ed0bb3856b2fed0b192417646
sllanten <dev.sllanten@gmail.com>
2025-05-07 09:29:10 -0500

    feat(validateMidware): se agrega nuevo Midware
    Se agrega el Midware "validateMidware" con la finalidad de redireccionar
    cuando el admin ya tiene una sesion iniciada e intenta ingresar a /admin.
    Ademas se implementa en el controller "AdminController"

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -5,6 +5,7 @@ class AdminController extends Controller
 {
     
     public function index(){
+        $this->validateMidware();
         $this->view('login', [
             'title' => 'Devsllanten',
             'css' => ['/assets/css/login.css'],
